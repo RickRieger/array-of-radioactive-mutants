1dcc33842bc18fb341faf19270a3dd561e3d12a0
Rick Rieger <rickrieger36251@gmail.com>
2021-03-15 10:22:31 -0400

    finished

--- main.js
+++ main.js
@@ -2,8 +2,44 @@
  * YOUR CODE HERE *
  ******************/
 
+function addMeToEnd(array){
+  array.push('Colin')
+}
+
+function addMeToStart(array){
+  array.unshift('Colin')
+}
+
+function changeLast(array, value){
+
+array[array.length - 1] = value
+
+}
+
+function changeAllValuesTo(array, value){
+for (i = 0; i<array.length; i++){
+     array[i] = value
+}
+}
+
+function oddOrEven(array){
+  for (i=0;i<array.length;i++){
+    if (array[i] % 2 === 0){
+      array[i]='even'
+    }
+    else{
+      array[i] ='odd'
+    }
+  }
+}
 
 
+function changeNextThreeToValue(num,array,givenValue){
+  let num2 = num + 3
+  for (let i=num;i< num2 ;i++){
+    array[i] = givenValue
+  }
+}
 
 
 /*********************************
